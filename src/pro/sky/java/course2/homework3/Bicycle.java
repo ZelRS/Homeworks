a214268071631e12e8654864f9b8fa19acd206fb
Roman Zelenin (ZelRS) <ZeleninRS1991@gmail.com>
2023-05-29 15:57:55 +0600

    Исходные блоки кода, которые необходимо поправить с учетом принципов ООП. Курс 2. Урок 3

---
+++ src/pro/sky/java/course2/homework3/Bicycle.java
@@ -0,0 +1,10 @@
+package pro.sky.java.course2.homework3;
+
+public class Bicycle {
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
